500b6b9c06380eaebdc1524a9b761b1c64fc9039
TanyaS08 <tanya.strydom@icloud.com>
2020-04-11 17:09:52 +0100

    New scripts related to data cleaning and exploration - to be edited

---
+++ scripts/Clean_species_&_site_data.R
@@ -0,0 +1,21 @@
+# Swedish Microclimate Project
+# Dalarna, Sweden - July 2019
+#
+# Authors: Ditte M. Christhansen, Kristoffer Hylander, 
+#          Tanya Strydom
+# Contact: tanya.strydom@icloud.com
+
+#' ------------------------------------------------------------------#
+#'  - This script is used to concatinate and clean the raw species
+#'    and site data collected in the field
+#' ------------------------------------------------------------------#
+
+#' ------------------------------------------------------------------#
+#'   TO DO:
+#'  - 
+#' ------------------------------------------------------------------#
+
+### 0) Preamble ----
+### >> a) Dependencies ----
+
+### End of script ----
---
+++ scripts/Data_exploration_and_variable_selection.R
@@ -0,0 +1,19 @@
+# Swedish Microclimate Project
+# Dalarna, Sweden - July 2019
+#
+# Authors: Ditte M. Christhansen, Kristoffer Hylander, 
+#          Tanya Strydom
+# Contact: tanya.strydom@icloud.com
+
+#' ------------------------------------------------------------------#
+#'  - This script is used for initial data exploration and
+#'    for variable selection to create a minimum dataset
+#' ------------------------------------------------------------------#
+
+#' ------------------------------------------------------------------#
+#'   TO DO:
+#'  - 
+#' ------------------------------------------------------------------#
+
+### 0) Preamble ----
+### >> a) Dependencies ----
