563ef7177ebf6bb098a8f8d2e93489753b211816
Tanya Strydom <tanya.strydom@icloud.com>
2020-04-17 10:55:22 +0100

    Added code for cleaning/prepping Calamgrostis and Barbilophoza data

--- scripts/Clean_species_&_site_data.R
+++ scripts/Clean_species_&_site_data.R
@@ -12,10 +12,70 @@
 
 #' ------------------------------------------------------------------#
 #'   TO DO:
-#'  - 
+#'  - #add total area for Linnea and Calamagrostis
 #' ------------------------------------------------------------------#
 
 ### 0) Preamble ----
 ### >> a) Dependencies ----
+library(tidyverse)
+library(tidylog)
+library(readxl)
+
+### 1) Environmental data ----
+
+env.raw <- read_excel(file.path("data_raw", "Env.xlsx"))
+
+### >> a) Environmental data ----
+
+
+### 2) Species data ----
+### >> a) Barbilophozia ----
+barbilophozia.raw <- read_excel(file.path("data_raw", "Dataset.xlsx"),
+                                sheet = "Barbilophozia")
+
+barbilophozia.clean <-
+  barbilophozia.raw %>%
+  #re-class variables
+  transmute(
+    Site = as.factor(Site),
+    Subpop = as.factor(Subpop),
+    Area = as.numeric(Area)) %>%
+  group_by(Site) %>% 
+  #this creates a variable that counts the number of subpopulations
+  add_tally() %>%
+  #sum variables to the site level
+  summarise(
+    #total area
+    Area = sum(Area),
+    #Number of subpopulations extracted from the tally as all values are the same for the site can
+    #calcualte the mean
+    Number_subpop = mean(n))
+
+#Write file
+write.csv(barbilophozia.clean,
+          file.path("data_processed", "barbilophozia.csv"))
+  
+### >> a) Calamagrostis ----
+calamagrostis.raw <- read_excel(file.path("data_raw", "Dataset.xlsx"),
+                                sheet = "Calamagrostis")
+
+calamagrostis.clean <-
+  calamagrostis.raw %>%
+  #re-class variables
+  mutate(
+    Site = as.factor(Site)) %>%
+  group_by(Site) %>% 
+  #this creates a variable that counts the number of subpopulations
+  add_tally() %>%
+  #sum variables to the site level
+  summarise(
+    #total area
+    ##TODO
+    #Number of subpopulations extracted from the tally as all values are the same for the site can
+    #calcualte the mean
+    Number_subpop = mean(n),
+    Number_flower = sum(Flower),
+    Number_cells = sum(Cells))
+
 
 ### End of script ----
