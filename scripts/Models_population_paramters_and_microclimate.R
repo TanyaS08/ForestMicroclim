fe29efcb04116e93be83c62e02e18b39e7713cfb
Tanya Strydom <tanya.strydom@icloud.com>
2020-04-18 10:54:19 +0100

    New script - for population parameters vs microclimate models

---
+++ scripts/Models_population_paramters_and_microclimate.R
@@ -0,0 +1,20 @@
+# Swedish Microclimate Project
+# Dalarna, Sweden - July 2019
+#
+# Authors: Ditte M. Christhansen, Kristoffer Hylander, 
+#          Tanya Strydom
+# Contact: tanya.strydom@icloud.com
+
+#' ------------------------------------------------------------------#
+#'  - This script runs the generalised linear models for species
+#'   population paramters and microclimate variables
+#' ------------------------------------------------------------------#
+
+#' ------------------------------------------------------------------#
+#'   TO DO:
+#' ------------------------------------------------------------------#
+
+### 0) Preamble ----
+### >> a) Dependencies ----
+
+### End of script ----
